3268ea8187bf26213b816907674d22acbc3ddadf
Riccardo <Coppola>
2020-04-19 00:23:35 +0100

    Adds number class to NumberTile and updates tests

--- src/components/number-tile/index.js
+++ src/components/number-tile/index.js
@@ -2,13 +2,14 @@ import classNames from "../../utils/class-names";
 
 const NumberTile = ({ ordinal, isSelected = false }) => {
   const classes = classNames({
+    number: true,
     selected: isSelected,
   });
 
   return `    
     <li data-role="number" data-number="${ordinal}" data-selected="${isSelected}" class="${classes}">
       ${ordinal}
-    </li>    
+    </li>  
   `;
 };
 
