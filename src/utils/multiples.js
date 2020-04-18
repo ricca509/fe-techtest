ceebd88473f4e6a8324d7841f58bf6b1353e42ac
Riccardo <Coppola>
2020-04-18 22:59:33 +0100

    Covers edge case for arguments in getMultiplesOf

--- src/utils/multiples.js
+++ src/utils/multiples.js
@@ -1,4 +1,8 @@
 const getMultiplesOf = (num, calculateUpToMaxOf) => {
+  if (!num || !calculateUpToMaxOf) {
+    throw new Error("Wrong arguments");
+  }
+
   const range = new Array(calculateUpToMaxOf)
     .fill()
     .map((_, index) => index + 1);
--- src/utils/multiples.spec.js
+++ src/utils/multiples.spec.js
@@ -1,6 +1,12 @@
 import getMultiplesOf from "./multiples";
 
 describe("getMultiplesOf", () => {
+  it("should throw if arguments are not correct", () => {
+    expect(() => {
+      getMultiplesOf();
+    }).toThrow();
+  });
+
   it("should return an array of all multiples of a number up to a max number", () => {
     // (Multiples are what we get after multiplying the number by an integer (not a fraction))
     const expected = [2, 4, 6, 8, 10];
