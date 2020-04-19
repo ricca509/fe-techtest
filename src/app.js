73b6d3a1ac703cf899406f82a63b4f712447926d
Riccardo <Coppola>
2020-04-19 09:28:25 +0100

    Implements and tests basic app implementation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
-const app = (maxNumberToShow) => {};
+import { renderToDom } from "./utils/dom";
+import Title from "./components/title";
+import NumberGrid from "./components/number-grid";
+
+const app = (maxNumberToShow) => {
+  const handleNumberSelection = (evt) => {};
+
+  const render = () => {
+    renderToDom(
+      [
+        Title(),
+        NumberGrid({
+          numbersToShow: maxNumberToShow,
+          selectedNumbers: [1, 2],
+          onSelection: handleNumberSelection,
+        }),
+      ],
+      document.querySelector("#app")
+    );
+  };
+
+  render();
+};
 
 export default app;
--- src/app.spec.js
+++ src/app.spec.js
@@ -1,7 +1,38 @@
 import app from "./app";
+import { renderToDom } from "./utils/dom";
+import Title from "./components/title";
+import NumberGrid from "./components/number-grid";
+import NumberTile from "./components/number-tile";
+
+jest.mock("./utils/dom");
+jest.mock("./components/title");
+jest.mock("./components/number-grid");
 
 describe("The application component", () => {
-  it("should render a title and a grid of boxes with numbers", () => {});
+  beforeEach(() => {
+    Title.mockImplementation(() => "<Title></Title>");
+    NumberGrid.mockImplementation(() => "<NumberGrid></NumberGrid>");
+    renderToDom.mockImplementation((inputs) => inputs);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render a title and a grid of boxes with numbers to a target element", () => {
+    app(2);
+
+    expect(renderToDom).toHaveBeenCalledTimes(1);
+    expect(renderToDom).toHaveBeenCalledWith(
+      ["<Title></Title>", "<NumberGrid></NumberGrid>"],
+      null
+    );
+    expect(NumberGrid).toHaveBeenCalledWith({
+      numbersToShow: 2,
+      selectedNumbers: [1, 2],
+      onSelection: expect.any(Function),
+    });
+  });
 
   it("when a number is clicked, it should select all its multiples", () => {});
 
